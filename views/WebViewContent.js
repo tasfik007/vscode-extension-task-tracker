3fa0f7d80036b2c9ae20390a8480b67ced413d39
tasfik <tasfik.rahman@marginedge.com>
2024-09-08 13:45:14 -0400

    Refactro view file

--- views/WebViewContent.js
+++ views/WebViewContent.js
@@ -1,8 +1,29 @@
 const { getWebviewUri } = require('../helpers/utilityMethods');
 
 function getWebviewContent(webView, context, todoListSavedData) {
-    const itemsHtml = todoListSavedData.items?.map(item => {
-        return `
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${todoListSavedData.label}</title>
+        <link rel="stylesheet" href="${getWebviewUri(webView, context.extensionUri, './resources/styles/main.css')}">
+      </head>
+      <body>
+        <div class="header">
+          <h2 class="title">${todoListSavedData.label}</h2>
+          <button id="addTodo" class="add-button">Add Task</button>
+        </div>
+        ${todoListSavedData.items?.map(item => TodoItem(item)).join('')}      
+        <script src="${getWebviewUri(webView, context.extensionUri, './controllers/extension.controller.js')}"></script>
+      </body>
+    </html>
+  `;
+}
+
+const TodoItem = (item) => {
+  return `
           <div class="todo-item ${item.checked ? 'checked' : ''}">
             <input type="checkbox" id="${item.id}" ${item.checked ? "checked" : ""} class="todo-checkbox">
             <label for="${item.id}" class="todo-label">${item.label}</label>
@@ -10,29 +31,8 @@ function getWebviewContent(webView, context, todoListSavedData) {
             <button class="deleteTask" id="${item.id}">✖</button>
           </div>
         `;
-    }).join('');
-
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>${todoListSavedData.label}</title>
-      <link rel="stylesheet" href="${getWebviewUri(webView, context.extensionUri, './resources/styles/main.css')}">
-    </head>
-    <body>
-      <div class="header">
-        <h2 class="title">${todoListSavedData.label}</h2>
-        <button id="addTodo" class="add-button">Add Task</button>
-      </div>
-      ${itemsHtml}      
-      <script src="${getWebviewUri(webView, context.extensionUri, './controllers/extension.controller.js')}"></script>
-    </body>
-    </html>
-  `;
 }
 
 module.exports = {
-    getWebviewContent
+  getWebviewContent
 };
